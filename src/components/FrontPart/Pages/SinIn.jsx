bce9341a9cd2ca863975b82c1503e2c83c2f843e
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-16 19:14:11 +0600

    upto date

--- src/components/FrontPart/Pages/SinIn.jsx
+++ src/components/FrontPart/Pages/SinIn.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useForm } from "react-hook-form";
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
-    // <!-- component -->
-    <div className="w-full py-5">
-      <form>
+    <div className="w-full  py-5">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
@@ -14,6 +22,7 @@ const SignIn = () => {
             type="text"
             placeholder="email"
             className="input input-bordered"
+            {...register("email", { required: true })}
           />
         </div>
         <div className="form-control">
@@ -24,30 +33,16 @@ const SignIn = () => {
             type="password"
             placeholder="password"
             className="input input-bordered"
+            {...register("password", { required: true })}
           />
-          <label className="label">
-            <Link
-              to="/forget-password"
-              className="label-text-alt link link-hover"
-            >
-              Forgot password?
-            </Link>
-          </label>
         </div>
         <div className="form-control mt-5">
           <button type="submit" className="btn btn-primary">
-            Login
+            Sign In
           </button>
         </div>
       </form>
-      <div className="form-control mt-6">
-        <p className="text-center">
-          Don't have an account yet?{" "}
-          <Link className="link" to="/register">
-            Sign up
-          </Link>
-        </p>
-      </div>
+      
     </div>
   );
 };

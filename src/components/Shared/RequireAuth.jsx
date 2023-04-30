6f1b2bca1fe3202eebb50525a229d84de53b193a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-04-30 17:59:52 +0600

    implement require auth

--- src/components/Shared/RequireAuth.jsx
+++ src/components/Shared/RequireAuth.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import AuthUser from "../../services/AuthUser";
+import useAuth from "../../customHooks/useAuth";
 
 const RequireAuth = ({ children }) => {
-  const { getToken } = AuthUser();
+  const { getToken, user } = AuthUser();
+  const { auth, error, loading } = useAuth(user?.email);
 
-  return getToken() ? children : <Navigate to="/sign-in" replace={true} />;
+  if (loading) {
+    return <p>loading...</p>;
+  }
+  if (Object.keys(auth).length) {
+    if (auth.data.success) {
+      return children;
+    }
+  }
+
+  return <Navigate to="/sign-in" replace={true} />;
 };
 
 export default RequireAuth;
--- src/customHooks/useAuth.js
+++ src/customHooks/useAuth.js
@@ -1,7 +1,30 @@
-const useAuth = () => {
-  const auth = true;
+import { useEffect, useState } from "react";
+import AuthUser from "../services/AuthUser";
 
-  return auth;
+const useAuth = (email) => {
+  const { http, getToken } = AuthUser();
+  const [auth, setAuth] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+   
+    try {
+      http.get("/auth").then((res) => {
+        setAuth(res);
+        setLoading(false);
+      });
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  }, [email]);
+
+  return {
+    auth,
+    error,
+    loading,
+  };
 };
 
 export default useAuth;
